0615b454d5ae56292ff84e75af30d6ab00e5351f
JuanSolo5 <juanscazon@gmail.com>
2022-11-15 15:08:31 -0300

    Agregue página principal

---
+++ paginaprincipal.php
@@ -0,0 +1,27 @@
+<html>
+
+<head>
+  <title>Pagina Principal</title>
+</head>
+
+<body>
+  Seleccione la operacion que desea realizar:
+  <br>
+  <form method="post" action="nuevalibreta.php">
+    <button type="submit">Nueva libreta</button>
+  </form>
+  <br>
+  <form method="post" action="buscarlibreta.php">
+    <button type="submit">Editar libreta</button>
+  </form>
+  <br>
+  <form method="post" action="sacarturno.php">
+    <button type="submit">Sacar turno</button>
+  </form>
+  <br>
+  <form method="post" action="obtenerlistadoturnos.php">
+    <button type="submit">Obtener listado de turnos</button>
+  </form>
+</body>
+
+</html>
