288889b1fcbe21730bfd85b0e8624006018ee595
JuanSolo5 <juanscazon@gmail.com>
2022-11-15 16:42:35 -0300

    añadida pagina de nueva libreta

---
+++ nuevalibreta.php
@@ -0,0 +1,28 @@
+<html>
+
+<head>
+  <title>Nueva Libreta</title>
+</head>
+
+<body>
+  Seleccione la operacion que desea realizar:
+  <br>
+  <br>
+  <form method="post" action="nuevalibreta.php">
+    <button type="submit">Nueva libreta</button>
+  </form>
+  <br>
+  <form method="post" action="buscarlibreta.php">
+    <button type="submit">Editar libreta</button>
+  </form>
+  <br>
+  <form method="post" action="sacarturno.php">
+    <button type="submit">Sacar turno</button>
+  </form>
+  <br>
+  <form method="post" action="obtenerlistadoturnos.php">
+    <button type="submit">Obtener listado de turnos</button>
+  </form>
+</body>
+
+</html>
